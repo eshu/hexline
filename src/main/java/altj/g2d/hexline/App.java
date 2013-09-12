33e247d728e303483600353492f8fbd3d72ba75c
Pavel Parkhomenko <pparkhomenko@usetech.ru>
2013-09-12 14:48:53 +0400

    init

---
+++ src/main/java/altj/g2d/hexline/App.java
@@ -0,0 +1,13 @@
+package altj.g2d.hexline;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
